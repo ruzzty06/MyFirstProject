d267a174d2dbd9ca0bf16c0d0182e63cf03f0281
ruzzty06 <ruzzty06@yahoo.com>
2017-05-08 00:31:03 +0100

    Initial Upload

---
+++ MvcApplication1/BLL/BLL.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data.Sql;
+
+namespace MvcApplication1
+{
+    public class BLL
+    {
+        
+        public int GetCount()
+        {
+            SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["dbCon"].ConnectionString);
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = con;
+            cmd.CommandText = "Select top 1 Cnt from dbo.Counter";
+            using (con)
+            {
+                con.Open();
+                return (int)cmd.ExecuteScalar();
+            }
+            
+        }
+        public void Increment()
+        {
+            SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["dbCon"].ConnectionString);
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = con;
+            cmd.CommandText = "Update dbo.Counter set cnt=cnt+1 where cnt<10";
+            using (con)
+            {
+                con.Open();
+                cmd.ExecuteNonQuery();
+            }
+        }
+    }
+}
---
+++ MvcApplication1/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+        [HttpGet]
+        public ActionResult Index()
+        {
+            
+            BLL bll = new BLL();
+            ViewData["Counter"] = bll.GetCount();
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(FormCollection collection)
+        {
+            
+                BLL bll = new BLL();
+                bll.Increment();
+                ViewData["Counter"] = bll.GetCount();
+           
+            return View();
+        }
+
+    }
+}
